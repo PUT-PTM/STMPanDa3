a3270347c847fa778665eb7e8fd6294b33de80f8
Mikolaj Drygas <mikolaj_drygas@tlen.pl>
2016-04-09 12:28:29 +0200

    drygi
    dodanie ocen 3,4,5 potem jeszcze zmienie te 2 zeby sie wyrozniala lepiej
    chyba bedzie...

---
+++ Assets/Destroy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        Destroy(collisionInfo.gameObject);
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
