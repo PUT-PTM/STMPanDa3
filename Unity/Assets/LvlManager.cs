10e3e9969b054b9b4ecd8080ad257344174b9774
Mikolaj Drygas <mikolaj_drygas@tlen.pl>
2016-05-14 20:04:10 +0200

    Gamepad moving, button for reset game, add folders STM and UNITY

---
+++ Unity/Assets/LvlManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LvlManager : MonoBehaviour
+{
+
+    public int score;
+    public int lives;
+    public GameObject restartText;
+    // Use this for initialization
+    void Start()
+    {
+        lives = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+   
+
+       if (lives <= 0)
+        {
+            // restartText.SetActive(true);
+
+            if (Input.GetButtonDown("Fire1"))
+                Application.LoadLevel(Application.loadedLevel);
+
+
+            Destroy(GameObject.Find("PanDa3"), 0);
+            return; 
+        }
+    }
+    public void AddPoints(int points)
+    {
+        score += points;
+    }
+    public void DecLives()
+    {
+        lives--;
+    }
+    private GUIStyle guiStyle = new GUIStyle(); //create a new variable
+
+    void OnGUI()
+    {
+        guiStyle.fontSize = 50;
+        GUI.contentColor = Color.black;
+        GUILayout.Label(score.ToString() + "\nlives:" + lives.ToString(), guiStyle);
+    }
+}
---
+++ Unity/Assets/PandaMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PandaMove : MonoBehaviour
+{
+    public float speed = 1; // speed in meters per second
+
+    void Update()
+    {
+        Vector3 moveDir = Vector3.zero;
+        moveDir.x = Input.GetAxis("Horizontal");
+        var rot = transform.rotation;
+        // get result of AD keys in X
+        // move this object at frame rate independent speed:
+
+        
+        
+        transform.position += moveDir * speed * Time.deltaTime;
+        
+        if(moveDir.x <0) transform.rotation = Quaternion.AngleAxis(0, Vector3.down); 
+        if (moveDir.x > 0) transform.rotation = Quaternion.AngleAxis(180, Vector3.down);
+
+
+
+
+
+
+    }
+
+
+}
